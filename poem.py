d85a11a3e67ac337b085b7379851d331be13890e
Vijayprasanna13 <vijayprasanna2896@gmail.com>
2017-08-26 21:03:10 +0530

    modify screenshot image

--- poem.py
+++ poem.py
@@ -15,19 +15,21 @@ def is_valid_line(line):
 	return False
 
 def get_poem():
-	page_no = random.randint(1,10)
-	page = urllib.urlopen('https://allpoetry.com/classics/famous_poems?page='+str(page_no))
-	page_content = page.read()
-	page_content = bs(page_content,"html.parser")
-	divs = page_content.find_all("div", {"class":"details"})
-	poem_index = random.randint(0,len(divs)-1)
-	poem_link = (divs[poem_index].contents)[0].attrs['href']
-	poem_page = urllib.urlopen('https://allpoetry.com'+poem_link)
-	poem_page_content = poem_page.read();
-	poem_page_content = bs(poem_page_content,"html.parser")
-	lines = poem_page_content.find('div',{'class':'preview poem_body'})
-	if len(lines) == 0:
-		get_poem
+	PASS = True
+	while PASS:
+		page_no = random.randint(1,10)
+		page = urllib.urlopen('https://allpoetry.com/classics/famous_poems?page='+str(page_no))
+		page_content = page.read()
+		page_content = bs(page_content,"html.parser")
+		divs = page_content.find_all("div", {"class":"details"})
+		poem_index = random.randint(0,len(divs)-1)
+		poem_link = (divs[poem_index].contents)[0].attrs['href']
+		poem_page = urllib.urlopen('https://allpoetry.com'+poem_link)
+		poem_page_content = poem_page.read();
+		poem_page_content = bs(poem_page_content,"html.parser")
+		lines = poem_page_content.find('div',{'class':'preview poem_body'})
+		if len(lines) != 0:
+			PASS = False		
 	title = ((poem_page_content.find('h1',{'class':'title'})).contents)[0].contents
 	poet = ((poem_page_content.find('span',{'class':'n'})).contents)[0].contents
 	del lines[-1]
